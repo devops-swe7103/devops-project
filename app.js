073ad6ec8b1df4707d37771efbde8ee91ea87c9a
ogaamyr <aamir@ogaamyr.com>
2021-12-24 12:33:23 +0100

    backed support for frontend scaffolding

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require(`path`);
 const mongoose = require(`mongoose`);
 const methodOverride = require("method-override");
+const morgan = require(`morgan`);
+
 const Property = require(`./models/property`);
 const Agent = require(`./models/agent`);
 
@@ -30,78 +32,58 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // override with POST having ?_method=DELETE/PATCH
 app.use(methodOverride("_method"));
+// using morgan to log request data
+app.use(morgan(`dev`));
 
 // route management
 app.get(`/`, (req, res) => {
-  console.log(`GET: /`);
-  // res.send(`GET: /`);
   res.render(`home`);
 });
 
 // property routes
 app.get(`/properties`, async (req, res) => {
-  console.log(`GET: /properties`);
   const properties = await Property.find({});
   const agents = await Agent.find({});
   res.render(`properties/index`, { properties, agents });
 });
 
 app.get(`/properties/add/`, (req, res) => {
-  console.log(`GET: /properties/add`);
-  // console.log(``)
   // res.send(`GET: /properties/add`);
   res.render(`properties/newPropertyForm`);
 });
 app.post(`/properties/add/`, async (req, res) => {
-  console.log(`POST: /properties/add`);
-
-  // console.log(`=== Recieved form data ===`);
   // processing the form submitted data to generate a property object
   const { generatePropertyFromForm } = require("./seeds/helpers");
   const newProperty = new Property({ ...generatePropertyFromForm(req.body) });
 
-  // console.log(`=== Saving Property Data to MongoDB ===`);
-  // adding new property to MongoDB
   await newProperty.save();
-  // console.log(`=== Save Successful ===`);
-
-  // console.log(`=== Redirecting to the newly added Property Page ===`);
-  res.redirect(`/properties/${newProperty._id}/`);
 });
 
 app.get(`/properties/:id`, async (req, res) => {
-  console.log(`GET: /properties/:id`);
-  // const { id } = req.params;
-
   const property = await Property.findById(req.params.id);
-  // // // console.log(`🚀 ✩ app.get ✩ property`, property);
 
   const agent = await Agent.findOne({ agentCode: property.agentCode });
-  // // // console.log(`🚀 ✩ app.get ✩ agent`, agent);
 
   res.render(`properties/showProperty`, { property, agent });
 });
 app.put(`/properties/:id`, async (req, res) => {
-  const { id } = req.params;
-  console.log(`🚀 ✩ app.put ✩ id`, id);
-  // const { id } = req.body;
-  // console.log(`🚀 ✩ app.put ✩ id`, id);
-
   const updatedProperty = await Property.findByIdAndUpdate(
     { _id: id },
     { ...req.body }
   );
-  console.log(`🚀 ✩ app.put ✩ updatedProperty`, updatedProperty);
   res.redirect(`/properties/${id}/`);
+});
+app.delete(`/properties/:id`, async (req, res) => {
+  const { id } = req.params;
+  await Property.findByIdAndDelete(id);
+  res.redirect(`/properties/`);
 });
 
 app.get(`/properties/:id/edit/`, async (req, res) => {
   const { id } = req.params;
-  console.log(`GET: /properties/${id}/edit/`);
 
   // getting the property data from the DB
   const property = await Property.findById(id);
-  // // console.log(`🚀 ✩ app.get ✩ property`, property);
 
   // res.send(`GET: /properties/${id}/edit/`);
   res.render(`properties/editPropertyForm`, { property });
@@ -109,22 +91,23 @@ app.get(`/properties/:id/edit/`, async (req, res) => {
 
 // agent routes
 app.get(`/agents`, async (req, res) => {
-  console.log(`GET: /agents`);
   const agents = await Agent.find({});
   res.render(`agents/index`, { agents });
 });
 
 app.get(`/agents/:id`, async (req, res) => {
-  console.log(`GET: /agents/:id`);
   const { id } = req.params;
 
   const agent = await Agent.findById(id);
-  // // // console.log(`🚀 ✩ app.get ✩ agent`, agent);
 
   const properties = await Property.find({ agentCode: agent.agentCode });
-  // // // console.log(`🚀 ✩ app.get ✩ properties`, properties);
 
   res.render(`agents/showAgent`, { agent, properties });
+});
+
+// 404 route
+app.use((req, res) => {
+  res.status(404).send("Resource not found");
 });
 
 // starting the server
